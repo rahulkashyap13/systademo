fd120c9d281cb96179bff473536fd1dc839f899d
rahulkashyap13 <rahulkashyap13>
2019-04-27 12:13:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+	<div className="App">
+		<header className="App-header">
+		  <Button variant="primary">Primary</Button>
+		</header>
+	</div>
+  );
+}
+
+export default App;
